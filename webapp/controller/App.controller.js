b6b540f009d14d44f49cd704e2566688f8cfe35f
Gedikli <Mustafa.Gedikli@bs.nttdata.com>
2024-07-06 17:45:53 +0300

    Using JSONModel

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => Controller.extend('ui5.horizon.controller.App', {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+
+], (Controller, MessageToast, JSONModel) => Controller.extend('ui5.horizon.controller.App', {
+    onInit() {
+        const oData = {
+            receipent: {
+                name:"Rafael"
+            },
+        };
+
+        const oModel = new JSONModel(oData);
+
+        this.getView().setModel(oModel);
+    },
     onPress() {
         MessageToast.show("HEllooooooooo", {
             duration: 500,
