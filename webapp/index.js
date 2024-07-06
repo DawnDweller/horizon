dc40d3be5eb8e404fa1b81c124a90e11cb9eb804
Gedikli <Mustafa.Gedikli@bs.nttdata.com>
2024-07-06 16:01:32 +0300

    declarative Text Rendering

--- webapp/index.js
+++ webapp/index.js
@@ -1,3 +1,15 @@
-sap.ui.define([], () => {
-    alert("UI5 is ready");
+sap.ui.define([
+    'sap/m/Text',
+], (Text) => {
+    new Text({
+        text: "Hello Mustafa"
+    }).placeAt('root');
+
+    const text = new Text ({
+        text: "Hello Alican"
+    });
+
+    console.log(text, Text);
+    text.placeAt("root")
+
 });
