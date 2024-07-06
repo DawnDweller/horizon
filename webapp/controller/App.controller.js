48fb3d8670d139ed00b2cc095e5a1b319cfd65ec
Gedikli <Mustafa.Gedikli@bs.nttdata.com>
2024-07-06 17:21:44 +0300

    Using Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => Controller.extend('ui5.horizon.controller.App', {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => Controller.extend('ui5.horizon.controller.App', {
     onPress() {
-        alert("HEllooooooooo");
+        MessageToast.show("HEllooooooooo", {
+            duration: 500,
+            onClose: () => MessageToast.show("Closed already :D"),
+        });
     },
 })
 
