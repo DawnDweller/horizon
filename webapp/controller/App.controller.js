ece1b3242ac72f5104e0a0ecf627ca64b89fb714
Gedikli <Mustafa.Gedikli@bs.nttdata.com>
2024-07-06 16:58:24 +0300

    Defining and Using Controller

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => Controller.extend('ui5.horizon.controller.App', {
+    onPress() {
+        alert("HEllooooooooo");
+    },
+})
+
+);
