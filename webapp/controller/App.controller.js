191a760d6a995fd237cf129b5bfb2d50a71cf75e
DawnDweller <mustafagedkli@gmail.com>
2024-07-08 00:42:24 +0300

    Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
 
-], (Controller, MessageToast, JSONModel) => Controller.extend('ui5.horizon.controller.App', {
+], (Controller, MessageToast, JSONModel, ResourceModel) => Controller.extend('ui5.horizon.controller.App', {
     onInit() {
         const oData = {
             receipent: {
@@ -14,11 +15,22 @@ sap.ui.define([
         const oModel = new JSONModel(oData);
 
         this.getView().setModel(oModel);
+
+        const i18nModel = new ResourceModel ({
+            bundleName: "ui5.horizon.i18n.i18n"
+        });
+        this.getView().setModel(i18nModel, "i18n");
     },
+
     onPress() {
-        MessageToast.show("HEllooooooooo", {
+        const i18nModel=this.getView().getModel("i18n").getResourceBundle();
+        const sReceipent=this.getView().getModel().getProperty("/receipent/name");
+        const sHelloMessage=i18nModel.getText("HelloMessage", [sReceipent]);
+        const sCloseMessage=i18nModel.getText("onCloseMessage", ["Foolishness", sReceipent]);
+            
+        MessageToast.show(sHelloMessage, {
             duration: 500,
-            onClose: () => MessageToast.show("Closed already :D"),
+            onClose: () => MessageToast.show(sCloseMessage),
         });
     },
 })
