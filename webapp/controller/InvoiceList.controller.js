27c90b08c7050c6b1d3975c29132b545d40ce319
DawnDweller <mustafagedkli@gmail.com>
2024-07-14 22:23:27 +0300

    Aggregation Binding_Correction
    Step_19 Correction on manifest.json

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,4 +1,4 @@
-sap.ui.define([
+/* sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel"
 ], (Controller, JSONModel) => {
@@ -11,4 +11,28 @@ sap.ui.define([
                     this.getView().setModel(oViewModel, "view");
             }
     });
-});
+}); */
+
+
+
+
+
+
+const dependencies= [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel"
+];
+function initializer(a,b) {
+        return a.extend("ui5.horizon.controller.InvoiceList", {
+                onInit() {
+                        const mustafa = new b({
+                                deneme1: "deneme2"
+                        });
+                        console.log(this, this.getView());
+                        this.getView().setModel(mustafa, "Dante");
+                }
+        });
+}
+        
+
+sap.ui.define(dependencies, initializer);
